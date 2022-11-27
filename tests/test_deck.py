3f50493a239d983a03125f44be1356e3a4f0c527
UsernameTaken420 <Nico_Gabin@yahoo.com>
2022-11-27 15:54:55 -0300

    added pytest tests

---
+++ tests/test_deck.py
@@ -0,0 +1,14 @@
+from .. import main
+import pytest
+
+# Arrange   
+@pytest.fixture
+def deck():
+    return main.generateDeck()
+
+def test_deck(deck):
+    assert type(deck) == list
+
+def test_draw(deck):
+    card = main.drawCard(deck)
+    assert (type(card) == tuple) and (type(card[0]) == str) and (type(card[1]) == str)
